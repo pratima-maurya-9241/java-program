d14b15deae2e0e3ab7c35d9aa8aa1f7ed05f127b
Pratima maurya <pratimamaurya83035@gmail.com>
2026-08-23 19:14:31 +0530

    add second program

---
+++ second.java
@@ -0,0 +1,21 @@
+public class second {
+    public static void main(String[]args){
+        int a=10;
+        int b=25;
+        int sum=a+b;
+        System.out.println(sum);
+        int diff=a-b;
+        System.out.println(diff);
+        int mul=a*b;
+        System.out.println(mul);
+        int div=a%b;
+        System.out.println(div);
+        int mod=a/b;
+        System.out.println(mod);
+
+
+
+    }
+
+    
+}
