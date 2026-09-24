1ab479e48ac164991e4eaf6626b5c3cf1320961f
pratima-maurya-9241 <pratimamaurya83035@gmail.com>
2026-09-24 15:41:41 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class even {
+    public static void main(String[]args){
+        Scanner Sc=new Scanner(System.in);
+        int x=Sc.nextInt();
+        if(x%2==0)
+        {
+            System.out.println("even no.");
+
+        }
+        else{
+            System.out.println("odd no.");
+        }
+
+    }
+    
+}
