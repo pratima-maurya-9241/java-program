18f053c75554d0090fc9cb68fc389de811bcaa6c
pratima-maurya-9241 <pratimamaurya83035@gmail.com>
2026-09-25 14:04:48 +0530

    Add files via upload

---
+++ greter_no.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class greter_no {
+    public static void main(String args[]){
+        Scanner Sc=new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+            if(a == b){
+                System.out.println("Equal");
+            }
+            else if(a > b){
+                System.out.println("a is greter");
+            }
+            else{
+                System.out.println("a is smaller");
+            }
+    }
+    
+}
